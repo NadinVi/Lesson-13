8017814306a1f8cca9e3dfbbdb5b78dc77f65108
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2023-01-03 12:31:26 +0100

    add task27

--- 27/script.js
+++ 27/script.js
@@ -1,16 +1,5 @@
 // Це ladder (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися:
-// let ladder = {
-//     step: 0,
-//     up: function () {
-//         this.step++;
-//     },
-//     down: function () {
-//         this.step--;
-//     },
-//     showStep: function () { // показывает текущую ступеньку
-//         alert(this.step);
-//     }
-// };
+// 
 // Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
 
 // ladder.up();
@@ -23,3 +12,35 @@
 
 // ladder.up().up().down().showStep(); // 1
 // Такий підхід широко використовується в бібліотеках JavaScript.
+
+let ladder = {
+        step: 0,
+        up: function () {
+            this.step++;
+            return this;
+        },
+        down: function () {
+            this.step--;
+            return this;
+        },
+        showStep: function () { // показывает текущую ступеньку
+            alert(this.step);
+            return this;
+        }
+    };
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 2
+
+ladder.up().up().down().showStep(); //1
+ladder.up().up().down().showStep(); //2
+ladder.up().up().down().showStep(); //3
+ladder.up().up().up().down().down().up().showStep();//5
